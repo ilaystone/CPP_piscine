80976c4afdae23a3b119f563568f12314d096027
Ilyass Khadem <ikhadem@e2r9p14.1337.ma>
2021-10-11 15:56:43 +0100

    started ex02 in c++

---
+++ DAY02/ex02/Fixed.cpp
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikhadem <ikhadem@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 11:46:21 by ikhadem           #+#    #+#             */
+/*   Updated: 2021/10/11 15:56:16 by ikhadem          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int const		Fixed::_bits = 8;
+
+Fixed::Fixed(void) : _value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+	return ;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+	return ;
+}
+
+Fixed::Fixed(int const v)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_value = (v << this->_bits);
+	return ;
+}
+
+Fixed::Fixed(float const v)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_value = (int)(std::roundf(v * (1 << this->_bits)));
+	return ;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+
+Fixed &		Fixed::operator=(Fixed const &rhs)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->_value = rhs.getRawBits();
+	return (*this);
+}
+
+bool		Fixed::operator>(Fixed const &rhs)
+{
+	if (this->getRawBits() > rhs.getRawBits())
+		return (true);
+	return (false);
+}
+
+bool		Fixed::operator<(Fixed const &rhs)
+{
+	if (this->getRawBits() < rhs.getRawBits())
+		return (true);
+	return (false);
+}
+
+
+int			Fixed::getRawBits() const
+{
+	// std::cout << "getRawBits member function called" << std::endl;
+	return (this->_value);
+}
+
+void		Fixed::setRawBits(int const raw)
+{
+	// std::cout << "setRawBits member function called" << std::endl;
+	this->_value = raw;
+	return ;
+}
+
+float		Fixed::toFloat(void) const
+{
+	return (((float)(this->_value) / (1 << _bits)));
+}
+
+int			Fixed::toInt(void) const
+{
+	return ((int)(this->_value >> _bits));
+}
+
+std::ostream&	operator<<(std::ostream& os, const Fixed &rhs)
+{
+	os << rhs.toFloat();
+	return (os);
+}
---
+++ DAY02/ex02/main.cpp
@@ -0,0 +1,27 @@
+	/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikhadem <ikhadem@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 11:45:09 by ikhadem           #+#    #+#             */
+/*   Updated: 2021/10/11 11:45:33 by ikhadem          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int		main()
+{
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	return (0);
+}
